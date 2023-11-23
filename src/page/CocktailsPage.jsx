f82848cb4f6b4319032cd3b8cab264eeccf4575d
Helene <helene.quintard@lapiscine.pro>
2023-11-23 16:37:20 +0100

    Add Cocktail's name

--- src/page/CocktailsPage.jsx
+++ src/page/CocktailsPage.jsx
@@ -10,26 +10,38 @@ const CocktailsPage = () => {
         return (
             response.json())
     })
-    .then ((json)=>{
-    setCocktails(json)
+    .then ((cocktailsInjson)=>{
+    setCocktails(cocktailsInjson.drinks)
     })
 }
 
     return (
         <main>
-        {cocktails?(<p>Cocktails prêts</p>
+        {cocktails? (
+        <>
+        {cocktails.map((cocktail)=>{
+            return (
+            <article>
+            <h2>{cocktail.strDrink}</h2>
+            </article>
+            );
+            })}
+        </>
         ):(
-        <p>Cocktails en cours de chargement</p>
+            <p>Cocktails en cours de chargement</p>
         )}
         </main>
     )
 }
 
+    
+    
 
-
+ 
 export default CocktailsPage;
 
 // J'utilise la fonction fetch afin de récupérer les données json de mon api. Comme c'est une fonction asynchrone, le then me permet d'attendre la réponse et ensuite de me renvoyer le fichier json.
 // J'utilise le hook useState afin de donner à ma variable la valeure initiale de null et la fonction setcocktails va modifier son état.
 //Du coup,si la variable est nul, j'affiche dans ma page Cocktails en cours de chargement et le composant va continuer à se recharger
 // Si le serveur a réussi à récupérer les données, j'affiche "Cocktails prêts" et le composant ne se rechargera plus
+//Dans mon return, j'utilise la fonction map afin de récupérer le tableau json et j'affiche cocktail.strDrink qui est le nom de mes cocktails
